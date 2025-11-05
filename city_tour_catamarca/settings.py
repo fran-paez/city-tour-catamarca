0a7957d84f111801ab43edd880ff50d9cb65a5f0
franpaez <aibarpaezfrancisco2015@gmail.com>
2025-11-04 23:54:30 -0300

    Comentarios y pruebas eliminadas

--- city_tour_catamarca/settings.py
+++ city_tour_catamarca/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-print("DB NAME:", config('POSTGRES_DB'))
-print("DEBUG:", config('DEBUG'))
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
